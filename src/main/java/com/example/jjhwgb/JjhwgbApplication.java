6c6bbe11a1e64f0d11cc62858e0074b1cf40d902
DarovskikhMV <doktormakc84@mail.ru>
2024-06-01 22:58:51 +0300

    first commit

---
+++ src/main/java/com/example/jjhwgb/JjhwgbApplication.java
@@ -0,0 +1,13 @@
+package com.example.jjhwgb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjhwgbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjhwgbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jjhwgb/JjhwgbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jjhwgb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjhwgbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
